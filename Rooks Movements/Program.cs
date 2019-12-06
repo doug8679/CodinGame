d5a5d50769b1c95cdf43d809a0cfe83ba7f5fdb5
Doug Braxton <Doug@deltasigmacompany.com>
2019-12-06 14:04:09 -0500

    Create project for Rooks Movements

---
+++ Rooks Movements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rooks_Movements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
