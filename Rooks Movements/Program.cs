ac418fd1f1ad8caa1b81396ee3ad20d5e3f765e4
Doug Braxton <Doug@deltasigmacompany.com>
2019-12-06 14:04:40 -0500

    Copy start proejct from codingame

--- Rooks Movements/Program.cs
+++ Rooks Movements/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Rooks_Movements
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string rookPosition = Console.ReadLine();
+        int nbPieces = int.Parse(Console.ReadLine());
+        for (int i = 0; i < nbPieces; i++)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputs = Console.ReadLine().Split(' ');
+            int colour = int.Parse(inputs[0]);
+            string onePiece = inputs[1];
         }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("ANSWER");
     }
 }
