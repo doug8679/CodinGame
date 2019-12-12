e53fd8eb424e3b52cf27ebf3f5060cd2bff962c1
Doug Braxton <Doug@deltasigmacompany.com>
2019-12-12 07:54:15 -0500

    Complete the HowTimeFlies solution

--- src/HowTimeFlies/Solution.cs
+++ src/HowTimeFlies/Solution.cs
@@ -8,26 +8,65 @@ using System.Collections.Generic;
 namespace HowTimeFlies
 {
 
-public class Solution
-{
-    DateTime _start, _end;
-    public Solution(string begin, string end) {
-        _start = DateTime.Parse(begin);
-        _end = DateTime.Parse(end);
-    }
-    public string Solve() {
-        var diff = _end - _start;
-        return $"{(int)(diff.TotalDays / 365)} years, total {diff.TotalDays} days";
-    }
-    static void Main(string[] args)
+    public class Solution
     {
-        string BEGIN = Console.ReadLine();
-        string END = Console.ReadLine();
+        DateTime _start, _end;
+        public Solution(string begin, string end)
+        {
+            var parts = begin.Split(".".ToCharArray());
+            _start = new DateTime(int.Parse(parts[2]), int.Parse(parts[1]), int.Parse(parts[0]));
+            parts = end.Split(".".ToCharArray());
+            _end = new DateTime(int.Parse(parts[2]), int.Parse(parts[1]), int.Parse(parts[0]));
+        }
+        public string Solve()
+        {
+            Console.Error.WriteLine($"Start: {_start}, End: {_end}");
+            var diff = _end - _start;
+            Console.Error.WriteLine($"Diff: {diff}");
+            DateTime next;
+            var years = CountYears(out next);
+            var months = CountMonths(next);
+            int days = (int)diff.TotalDays;
+            Console.Error.WriteLine($"Years: {years}, Months: {months}, Days: {days}");
+            string result = "";
+            if (years > 0)
+            {
+                result += $"{years} year{((years > 1)?"s":"")}, ";
+            }
+            if (months > 0)
+            {
+                result += $"{months} month{((months > 1)?"s":"")}, ";
+            }
+            return result + $"total {days} days";
+        }
 
-        // Write an action using Console.WriteLine()
-        // To debug: Console.Error.WriteLine("Debug messages...");
+        private int CountYears(out DateTime next)
+        {
+            int result = 0;
+            while (_start.AddYears(result) <= _end)
+            {
+                result++;
+            }
+            next = _start.AddYears(result - 1);
+            return result-1;
+        }
+        private int CountMonths(DateTime start)
+        {
+            int result = 0;
+            while (start.AddMonths(result) <= _end)
+            {
+                result++;
+            }
+            return result - 1;
+        }
+        static void Main(string[] args)
+        {
+            string BEGIN = Console.ReadLine();
+            string END = Console.ReadLine();
 
-        Console.WriteLine("YY year[s], MM month[s], total NN days");
+            var sln = new Solution(BEGIN, END);
+
+            Console.WriteLine(sln.Solve());
+        }
     }
-}
 }
--- tests/HowTimeFliesTests/HowTimeFliesTests.cs
+++ tests/HowTimeFliesTests/HowTimeFliesTests.cs
@@ -20,42 +20,58 @@ namespace HowTimeFliesTests
         [Test]
         public void FullMonths()
         {
-            Assert.Pass();
+            var sln = new Solution("01.01.2016", "01.08.2016");
+            var answer = sln.Solve();
+            Assert.AreEqual("7 months, total 213 days", answer);
         }
         [Test]
         public void YearsAndMonths()
         {
-            Assert.Pass();
+            var sln = new Solution("01.11.2015", "01.02.2017");
+            var answer = sln.Solve();
+            Assert.AreEqual("1 year, 3 months, total 458 days", answer);
         }
         [Test]
         public void DaysOnly()
         {
-            Assert.Pass();
+            var sln = new Solution("17.12.2016", "16.01.2017");
+            var answer = sln.Solve();
+            Assert.AreEqual("total 30 days", answer);
         }
         [Test]
         public void SameDate()
         {
-            Assert.Pass();
+            var sln = new Solution("01.01.2016", "01.01.2016");
+            var answer = sln.Solve();
+            Assert.AreEqual("total 0 days", answer);
         }
         [Test]
         public void Complex()
         {
-            Assert.Pass();
+            var sln = new Solution("28.02.2015", "13.04.2018");
+            var answer = sln.Solve();
+            Assert.AreEqual("3 years, 1 month, total 1140 days", answer);
         }
         [Test]
         public void OneMonth()
         {
-            Assert.Pass();
+            var sln = new Solution("28.01.2015", "28.02.2015");
+            var answer = sln.Solve();
+            Assert.AreEqual("1 month, total 31 days", answer);
         }
         [Test]
         public void OneYear()
         {
-            Assert.Pass();
+            var sln = new Solution("17.03.2022", "17.03.2023");
+            var answer = sln.Solve();
+            Assert.AreEqual("1 year, total 365 days", answer);
         }
         [Test]
         public void LeapYear()
         {
-            Assert.Pass();
+            var sln = new Solution("17.02.2024", "17.02.2025");
+            var answer = sln.Solve();
+            Assert.AreEqual("1 year, total 366 days", answer);
         }
     }
 }
