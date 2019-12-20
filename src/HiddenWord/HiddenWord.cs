33b9bb9552d8c34a77b557954fe11ba668bc6747
Doug Braxton <db250583@ncr.com>
2019-12-20 15:51:40 -0500

    Updated implementation and unit tests
    Added all test cases, made classes uniform for testing

--- src/HiddenWord/HiddenWord.cs
+++ src/HiddenWord/HiddenWord.cs
@@ -12,21 +12,87 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void HorizontalWords()
         {
-            var grid = new WordGrid(3,3);
-                grid.SetRow("BAC", 0);
-                grid.SetRow("BOB", 1);
-                grid.SetRow("RED", 2);
+            var grid = new WordGrid(3,3, new string[]
+            {
+                "BAC", "BOB", "RED"
+            });
             var finder = new WordFinder(new List<string>{"BAC","BOB"}, grid);
-            Assert.Pass();
+            Assert.AreEqual("RED", finder.Find());
+        }
+
+        [Test]
+        public void VerticalWords()
+        {
+            var grid = new WordGrid(4, 4, new string[]
+            { "GTRI", "LMAL", "UAGO", "ECML"});
+            var finder = new WordFinder(new List<string> { "GLUE", "RAG", "LOL", "MAC" }, grid);
+            Assert.AreEqual("TIM", finder.Find());
+        }
+
+        [Test]
+        public void AllDirectionsSmall()
+        {
+            var grid = new WordGrid(3, 3, new string[] {"BAC", "EOO", "CKB"});
+            var finder = new WordFinder(new List<string> { "BAC", "BOB", "BEC", "COC" }, grid);
+            Assert.AreEqual("OK", finder.Find());
+        }
+
+        [Test]
+        public void AllDirectionsBig()
+        {
+            var grid = new WordGrid(12, 12, new string[]
+            {
+                "VCENJUGUENTO",
+                "OCORCSEERRAN",
+                "SOCHASSIGNET",
+                "SDRAHCUAFDND",
+                "AENMALMENEES",
+                "BSETINMUIDEM",
+                "VOLLEYBALLTG",
+                "COLATURESAUM",
+                "ISEMAEGROGER",
+                "SSURSITESERE",
+                "TEGNITRIFIAT",
+                "ENILECOUSAIS"
+            });
+            grid.SetRow("BAC", 0);
+            grid.SetRow("EOO", 1);
+            grid.SetRow("CKB", 2);
+            var finder = new WordFinder(new List<string>
+            {
+                "BASSOV",
+                "CELINE",
+                "CHASSIGNET",
+                "CISTE",
+                "COLATURES",
+                "COUSAIS",
+                "EAMES",
+                "ENJUGUENT",
+                "ERES",
+                "ESCROC",
+                "FAUCHARDS",
+                "GRAUBUNDEN",
+                "INSERAIENT",
+                "MALMENEES",
+                "MEDIUMNITES",
+                "NARREES",
+                "NITRIFIAT",
+                "ODES",
+                "REGORGEA",
+                "SURSITES",
+                "TUERA",
+                "VOLLEYBALL"
+            }, grid);
+            Assert.AreEqual("OK", finder.Find());
         }
     }
 
     public class WordGrid {
         public char[,] grid;
 
-        public WordGrid(int w, int h)
+        public WordGrid(int w, int h, string[] rows)
         {
             grid = new char[w, h];
         }
@@ -46,6 +112,11 @@ namespace Tests
         {
             _grid = grid;
             _words = list;
+        }
+
+        public string Find()
+        {
+            return string.Empty;
         }
     }
 }
