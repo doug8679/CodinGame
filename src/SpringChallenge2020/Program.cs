4235a44e81c846fd85fcc809e4a1e84877a03d7f
db250583 <Doug.Braxton@ncr.com>
2020-05-11 10:59:30 -0400

    Second submission
    Refined the super pellet approach to _actually_ compute
    closest pellet and go there.

--- src/SpringChallenge2020/Program.cs
+++ src/SpringChallenge2020/Program.cs
@@ -28,13 +28,16 @@ class Player
             int myScore = int.Parse(inputs[0]);
             int opponentScore = int.Parse(inputs[1]);
             int visiblePacCount = int.Parse(Console.ReadLine()); // all your pacs and enemy pacs in sight
+            int myX=0, myY=0;
             for (int i = 0; i < visiblePacCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int pacId = int.Parse(inputs[0]); // pac number (unique within a team)
                 bool mine = inputs[1] != "0"; // true if this pac is yours
-                int x = int.Parse(inputs[2]); // position in the grid
-                int y = int.Parse(inputs[3]); // position in the grid
+                if (mine) {
+                   myX = int.Parse(inputs[2]); // position in the grid
+                   myY = int.Parse(inputs[3]); // position in the grid
+                }
                 string typeId = inputs[4]; // unused in wood leagues
                 int speedTurnsLeft = int.Parse(inputs[5]); // unused in wood leagues
                 int abilityCooldown = int.Parse(inputs[6]); // unused in wood leagues
@@ -42,12 +45,13 @@ class Player
             int visiblePelletCount = int.Parse(Console.ReadLine()); // all pellets in sight
             List<Tuple<int, int>> superPellets = new List<Tuple<int, int>>();
             Tuple<int, int> nextPos = null;
+            double dist = double.MaxValue;
             for (int i = 0; i < visiblePelletCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int x = int.Parse(inputs[0]);
                 int y = int.Parse(inputs[1]);
-                if (nextPos == null) {
+                if (GetDistance(myX, myY, x, y) < dist) {
                     nextPos = new Tuple<int,int>(x, y);
                 }
                 int value = int.Parse(inputs[2]); // amount of points this pellet is worth
@@ -58,10 +62,20 @@ class Player
 
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
-            if (superPellets.Any())
-                nextPos = superPellets[0];
+            double superDist = double.MaxValue;
+            foreach (var pellet in superPellets) {
+                if (GetDistance(myX, myY, pellet.Item1, pellet.Item2) < superDist) {
+                    nextPos = pellet;
+                }
+            }
             Console.WriteLine($"MOVE 0 {nextPos.Item1} {nextPos.Item2}"); // MOVE <pacId> <x> <y>
 
         }
+    }
+
+    static double GetDistance(int x1, int y1, int x2, int y2) {
+        var x = Math.Pow(x2 - x1, 2);
+        var y = Math.Pow(y2 - y1, 2);
+        return Math.Sqrt(x + y);
     }
 }
