ff349ab17fa02a46300894e49d6961d5f99ae2ec
db250583 <Doug.Braxton@ncr.com>
2020-05-11 11:02:34 -0400

    Third submission
    Controlling multiple Pacs, but determine closest high-value object to each Pac.  Still prefers super pellets
    but will consider regular ones if nothing avaialble.

--- src/SpringChallenge2020/Program.cs
+++ src/SpringChallenge2020/Program.cs
@@ -16,9 +16,13 @@ class Player
         inputs = Console.ReadLine().Split(' ');
         int width = int.Parse(inputs[0]); // size of the grid
         int height = int.Parse(inputs[1]); // top left corner is (x=0, y=0)
+        char[,] map = new char[height, width];
         for (int i = 0; i < height; i++)
         {
-            string row = Console.ReadLine(); // one line of the grid: space " " is floor, pound "#" is wall
+            var line = Console.ReadLine();
+            for (int j=0; j < width; j++) {
+                map[i, j] = line[j];
+            }
         }
 
         // game loop
@@ -28,32 +32,32 @@ class Player
             int myScore = int.Parse(inputs[0]);
             int opponentScore = int.Parse(inputs[1]);
             int visiblePacCount = int.Parse(Console.ReadLine()); // all your pacs and enemy pacs in sight
-            int myX=0, myY=0;
+            List<Pac> myPacs = new List<Pac>();
             for (int i = 0; i < visiblePacCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int pacId = int.Parse(inputs[0]); // pac number (unique within a team)
                 bool mine = inputs[1] != "0"; // true if this pac is yours
+                int x = int.Parse(inputs[2]);
+                int y = int.Parse(inputs[3]);
                 if (mine) {
-                   myX = int.Parse(inputs[2]); // position in the grid
-                   myY = int.Parse(inputs[3]); // position in the grid
+                    myPacs.Add(new Pac(pacId, x, y));
                 }
                 string typeId = inputs[4]; // unused in wood leagues
                 int speedTurnsLeft = int.Parse(inputs[5]); // unused in wood leagues
                 int abilityCooldown = int.Parse(inputs[6]); // unused in wood leagues
             }
             int visiblePelletCount = int.Parse(Console.ReadLine()); // all pellets in sight
+            List<Tuple<int, int>> pellets = new List<Tuple<int, int>>();
             List<Tuple<int, int>> superPellets = new List<Tuple<int, int>>();
-            Tuple<int, int> nextPos = null;
             double dist = double.MaxValue;
             for (int i = 0; i < visiblePelletCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int x = int.Parse(inputs[0]);
                 int y = int.Parse(inputs[1]);
-                if (GetDistance(myX, myY, x, y) < dist) {
-                    nextPos = new Tuple<int,int>(x, y);
-                }
+                pellets.Add(new Tuple<int,int>(x, y));
+                
                 int value = int.Parse(inputs[2]); // amount of points this pellet is worth
                 if (value > 1) {
                     superPellets.Add(new Tuple<int, int>(x, y));
@@ -62,13 +66,19 @@ class Player
 
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
+            List<string> commands = new List<string>();
+            foreach (var pac in myPacs) {
+                Tuple<int, int> pos = pac.GetClosestTarget(superPellets, pellets);
+                commands.Add($"MOVE {pac.Id} {pos.Item1} {pos.Item2}");
+            }
             double superDist = double.MaxValue;
-            foreach (var pellet in superPellets) {
+            /*foreach (var pellet in superPellets) {
                 if (GetDistance(myX, myY, pellet.Item1, pellet.Item2) < superDist) {
                     nextPos = pellet;
                 }
-            }
-            Console.WriteLine($"MOVE 0 {nextPos.Item1} {nextPos.Item2}"); // MOVE <pacId> <x> <y>
+            }*/
+            Console.WriteLine(string.Join("|", commands)); // MOVE <pacId> <x> <y>
+            //Console.WriteLine("MOVE 0 0 0");
 
         }
     }
@@ -77,5 +87,46 @@ class Player
         var x = Math.Pow(x2 - x1, 2);
         var y = Math.Pow(y2 - y1, 2);
         return Math.Sqrt(x + y);
+    }
+}
+
+class Pac {
+    public int Id {get;set;}
+    public int X {get;set;}
+    public int Y {get;set;}
+
+    public Pac(int id, int x, int y) {
+        Id = id;
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString() {
+        return $"Pac {Id} @ ({X}, {Y})";
+    }
+
+    public Tuple<int, int> GetClosestTarget(List<Tuple<int, int>> super, List<Tuple<int, int>> reg) {
+        var minDist = double.MaxValue;
+        Tuple<int, int> result = null;
+        if (super.Any()) {
+            foreach (var targ in super) {
+                var dist = Math.Sqrt(Math.Pow(X-targ.Item1,2) + Math.Pow(Y-targ.Item2,2));
+                if (dist < minDist) {
+                    result = targ;
+                    minDist = dist;
+                }
+            }
+            super.Remove(result);
+        } else {
+            foreach(var targ in reg) {
+                var dist = Math.Sqrt(Math.Pow(X-targ.Item1,2) + Math.Pow(Y-targ.Item2,2));
+                if (dist < minDist) {
+                    result = targ;
+                    minDist = dist;
+                }
+            }
+            reg.Remove(result);
+        }
+        return result;
     }
 }
