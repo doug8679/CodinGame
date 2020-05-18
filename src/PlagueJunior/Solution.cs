ef4c23d424595dbe5c55455aef5539eb601dd21c
db250583 <Doug.Braxton@ncr.com>
2020-05-18 14:36:02 -0400

    Add starting code for Plague, Jr.

---
+++ src/PlagueJunior/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PlagueJunior
+{
+    /**
+     * Auto-generated code below aims at helping you parse
+     * the standard input according to the problem statement.
+     **/
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int A = int.Parse(inputs[0]);
+                int B = int.Parse(inputs[1]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("42");
+        }
+    }
+}
