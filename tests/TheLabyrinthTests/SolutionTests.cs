e121545f7ffae7d1cca2c890b0897cfed57f5204
Doug Braxton <doug8679@users.noreply.github.com>
2021-11-08 13:57:10 -0500

    Add Labyrinth puzzle to solution

---
+++ tests/TheLabyrinthTests/SolutionTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TheLabyrinthTests
+{
+    public class SolutionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
